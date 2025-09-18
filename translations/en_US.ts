839efab596cac05c122ac852aed56d45ddb8cfcd
George <161114343+G1VC@users.noreply.github.com>
2025-09-18 16:22:45 +0500

    Add files via upload

---
+++ translations/en_US.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Chat 5.0</source>
+        <translation>Chat 5.0</translation>
+    </message>
+    <message>
+        <source>Users</source>
+        <translation>Users</translation>
+    </message>
+    <message>
+        <source>Messages</source>
+        <translation>Messages</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Send</translation>
+    </message>
+</context>
+<context>
+    <name>LoginForm</name>
+    <message>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Username</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+</context>
+<context>
+    <name>RegistrationForm</name>
+    <message>
+        <source>Register</source>
+        <translation>Register</translation>
+    </message>
+    <message>
+        <source>Confirm Password</source>
+        <translation>Confirm Password</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/ru_RU.ts
@@ -0,0 +1,53 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Chat 5.0</source>
+        <translation>CHat 5.0</translation>
+    </message>
+    <message>
+        <source>Users</source>
+        <translation>Pol'zovateli</translation>
+    </message>
+    <message>
+        <source>Messages</source>
+        <translation>Soobshcheniya</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Otpravit'</translation>
+    </message>
+</context>
+<context>
+    <name>LoginForm</name>
+    <message>
+        <source>Login</source>
+        <translation>Vhod</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Imya pol'zovatelya</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Parol'</translation>
+    </message>
+</context>
+<context>
+    <name>RegistrationForm</name>
+    <message>
+        <source>Register</source>
+        <translation>Zaregistrirovat'sya</translation>
+    </message>
+    <message>
+        <source>Confirm Password</source>
+        <translation>Podtverzhdeniye parolya</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+</context>
+</TS>
